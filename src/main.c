4b68db100900c28ffc2315df45333626cd4280a4
Hadrian Hughes <hadrian.hughes@and.digital>
2021-10-06 14:39:24 +0100

    Store a list of pixel structs for each pixel in bitmap

--- src/main.c
+++ src/main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include "../lib/cbmp.h"
 
+typedef struct Pixel {
+  unsigned char r, g, b;
+} Pixel;
+
 int main() {
   BMP *bmp = bopen("example.bmp");
 
   unsigned int width = get_width(bmp);
   unsigned int height = get_height(bmp);
-  unsigned char r, g, b;
+  Pixel pixels[width * height];
+  unsigned int pxHead = 0;
 
   for (int x = 0;x < width;x++) {
     for (int y = 0;y < height;y++) {
+      unsigned char r, g, b;
       get_pixel_rgb(bmp, x, y, &r, &g, &b);
-      printf("%u %u %u\n", r, g, b);
+
+      Pixel p;
+      p.r = r;
+      p.g = g;
+      p.b = b;
+
+      pixels[pxHead] = p;
+      pxHead++;
     }
   }
 
